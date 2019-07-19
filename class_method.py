4a40cd116c6a59e552d55589ff13edacf6b768c4
amirkhan1092 <amirkhan1092@gmail.com>
2019-07-19 10:44:49 +0530

    direct by class attribute

--- class_method.py
+++ class_method.py
@@ -1,4 +1,5 @@
 class Employee:
+
     def __init__(self, pay, age):
         self.pay = pay
         self.age = age
@@ -12,6 +13,12 @@ class Employee:
         pay, age = s.split()
         return cls(pay,age)
 
+    @classmethod
+    def common_clear(cls):
+        cls.pay = 0
+        cls.age = 0
+        return cls
+
 
 em1 = Employee(2000, 24)
 # em2 = Employee(1000, 25)
@@ -20,7 +27,7 @@ em2 = Employee.from_string('1000 25')
 print(em1.age)
 print(em2.age)
 
-em1.update()
+Employee.common_clear()
 
-print(em1.age)
+print(Employee.age)
 print(em2.age)
