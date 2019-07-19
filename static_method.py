86ecdcab8c921a839ed762225e19425d93de87a1
amirkhan1092 <amirkhan1092@gmail.com>
2019-07-19 10:50:43 +0530

    static method applying as independent version to use the method without instance

---
+++ static_method.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self, name, pay):
+        self.name = name
+        self.pay = pay
+    def print_data(self):
+        print('Name ', self.name, 'Pay ', self.pay)
+        return None
+    @staticmethod
+    def independent(value):
+        print('value receiving from user ', value)
+        return value+34
+
+
+emp1 = Employee('vinay',3433)
+print(emp1.independent(89))
+
